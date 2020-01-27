2ad654aa2bd16d7e15614af7dbd57f596bdd0dbd
Restyled.io <commits@restyled.io>
2020-01-27 22:36:35 +0000

    Restyled by clang-format

--- docs/cli_kit/ansi/color_codes/sidebar-items.js
+++ docs/cli_kit/ansi/color_codes/sidebar-items.js
@@ -1,1 +1,13 @@
-initSidebarItems({"fn":[["green","Take an item that implements ToString and return in green"],["green_bold","Take an item that implements ToString and return in green bold"],["red","Take an item that implements ToString and return in red"],["red_bold","Take an item that implements ToString and return in red bold"]]});
+initSidebarItems({
+  "fn" : [
+    [ "green", "Take an item that implements ToString and return in green" ],
+    [
+      "green_bold",
+      "Take an item that implements ToString and return in green bold"
+    ],
+    [ "red", "Take an item that implements ToString and return in red" ],
+    [
+      "red_bold", "Take an item that implements ToString and return in red bold"
+    ]
+  ]
+});
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,3 +1,8 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["cli_kit"] = {"name":"","dirs":[{"name":"ansi","files":["color_codes.rs","mod.rs"]}],"files":["lib.rs"]};
+var N = null;
+var sourcesIndex = {};
+sourcesIndex["cli_kit"] = {
+  "name" : "",
+  "dirs" : [ {"name" : "ansi", "files" : [ "color_codes.rs", "mod.rs" ]} ],
+  "files" : [ "lib.rs" ]
+};
 createSourceSidebar();
