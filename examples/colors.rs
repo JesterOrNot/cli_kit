fdfabe8675e3d5a2384b2d15a352ebe64ab55973
Sean Hellum <seanhellum45@gmail.com>
2020-01-27 23:31:31 +0000

    deglob import :sparkles:

--- examples/colors.rs
+++ examples/colors.rs
@@ -1,4 +1,4 @@
-use cli_kit::ansi::color_codes::*;
+use cli_kit::ansi::color_codes::{green, green_bold, red, red_bold};
 
 fn main() {
     println!("{} {} {} {}", red(5), red_bold(5), green(5), green_bold(5));
