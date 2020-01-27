d9b951d7b8580660317c248cc3ce9aea91ad16b4
Sean Hellum <seanhellum45@gmail.com>
2020-01-27 22:27:00 +0000

    add docs

---
+++ docs/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["lib"] = {"name":"","dirs":[{"name":"ansi","files":["color_codes.rs","mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
--- src/ansi/color_codes.rs
+++ src/ansi/color_codes.rs
@@ -1,17 +1,65 @@
 use std::string::ToString;
 
+/// Take an item that implements ToString and return in red
+///
+/// Example
+///
+/// ```rust
+///
+/// use cli_kit::ansi::color_codes::red;
+///
+/// fn main() {
+///     println!("{}", red(5));
+/// }
+/// ```
 pub fn red<T: ToString>(text: T) -> String {
     "\x1b[31m".to_owned() + &text.to_string() + "\x1b[0m"
 }
 
+/// Take an item that implements ToString and return in red bold
+///
+/// Example
+///
+/// ```rust
+///
+/// use cli_kit::ansi::color_codes::red_bold;
+///
+/// fn main() {
+///     println!("{}", red_bold(5));
+/// }
+/// ```
 pub fn red_bold<T: ToString>(text: T) -> String {
     "\x1b[1;31m".to_owned() + &text.to_string() + "\x1b[0m"
 }
 
+/// Take an item that implements ToString and return in green
+///
+/// Example
+///
+/// ```rust
+///
+/// use cli_kit::ansi::color_codes::green;
+///
+/// fn main() {
+///     println!("{}", green(5));
+/// }
+/// ```
 pub fn green<T: ToString>(text: T) -> String {
     "\x1b[32m".to_owned() + &text.to_string() + "\x1b[0m"
 }
 
+/// Take an item that implements ToString and return in green bold
+///
+/// Example
+///
+/// ```rust
+///
+/// use cli_kit::ansi::color_codes::green_bold;
+///
+/// fn main() {
+///     println!("{}", green_bold(5));
+/// }
+/// ```
 pub fn green_bold<T: ToString>(text: T) -> String {
     "\x1b[1;32m".to_owned() + &text.to_string() + "\x1b[0m"
 }
