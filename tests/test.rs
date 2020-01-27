ca3734d14bb278a1aafefd6e8544b58eec6f18d4
Sean Hellum <seanhellum45@gmail.com>
2020-01-27 23:16:26 +0000

    add more unit tests :hammer:

--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,20 @@
 #[cfg(test)]
 mod tests {
-    use cli_kit::ansi::color_codes::red;
+    use cli_kit::ansi::color_codes::*;
+    #[test]
+    fn test_red() {
+        assert_eq!(red(5), "\x1b[31m5\x1b[0m");
+    }
     #[test]
     fn test_red_bold() {
-        assert_eq!(red(5), "\x1b[31m5\x1b[0m");
+        assert_eq!(red_bold(5), "\x1b[1;31m5\x1b[0m");
+    }
+    #[test]
+    fn test_green() {
+        assert_eq!(green(5), "\x1b[32m5\x1b[0m");
+    }
+    #[test]
+    fn test_green_bold() {
+        assert_eq!(green_bold(5), "\x1b[1;32m5\x1b[0m");
     }
 }
