8bb30df4e56ed4f97e0a79aa47818995f9caf2f9
Sean Hellum <seanhellum45@gmail.com>
2020-01-27 21:58:28 +0000

    add more colors

--- examples/hi.rs
+++ examples/hi.rs
@@ -1,4 +1,4 @@
 use cli_kit;
 fn main() {
-    println!("{}", cli_kit::ansi::color_codes::red(5));
+    println!("{} {}", cli_kit::ansi::color_codes::red(5),cli_kit::ansi::color_codes::red_bold(5));
 }
--- src/ansi/color_codes.rs
+++ src/ansi/color_codes.rs
@@ -3,3 +3,16 @@ use std::string::ToString;
 pub fn red<T: ToString>(text: T) -> String {
     "\x1b[31m".to_owned() + &text.to_string() + "\x1b[0m"
 }
+
+pub fn red_bold<T: ToString>(text: T) -> String {
+    "\x1b[1;31m".to_owned() + &text.to_string() + "\x1b[0m"
+}
+
+
+pub fn green<T: ToString>(text: T) -> String {
+    "\x1b[32m".to_owned() + &text.to_string() + "\x1b[0m"
+}
+
+pub fn green_bold<T: ToString>(text: T) -> String {
+    "\x1b[1;32m".to_owned() + &text.to_string() + "\x1b[0m"
+}
