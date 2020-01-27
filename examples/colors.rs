7c0f4d1c2ed82c3638a51b1a5d2660afc38835c3
Sean Hellum <seanhellum45@gmail.com>
2020-01-27 22:46:14 +0000

    change stuff

--- examples/colors.rs
+++ examples/colors.rs
@@ -2,5 +2,5 @@ use cli_kit::ansi::color_codes::*;
 
 fn main() {
     println!("{} {} {} {}", red(5), red_bold(5), green(5), green_bold(5));
-    println!("{}", green("Hello World From Rust!"))
+    println!("{}", green_bold("Hello World From Rust!"))
 }
