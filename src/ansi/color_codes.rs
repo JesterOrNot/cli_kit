f64a9a60b75e85038dad4102b84da9d26c2a28e0
Sean Hellum <seanhellum45@gmail.com>
2020-01-27 21:39:37 +0000

    format and use implicit return

--- src/ansi/color_codes.rs
+++ src/ansi/color_codes.rs
@@ -1,4 +1,3 @@
-pub fn Red<T: std::string::ToString>(text: T) -> String {
-    
-    return "\x1b[31m".to_owned() + &text.to_string() + "\x1b[0m"
+pub fn Red<T: std::string::ToString>(text: T) -> String {   
+    "\x1b[31m".to_owned() + &text.to_string() + "\x1b[0m"
 }
