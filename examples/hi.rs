ed4b3af82242e130263379c5df5c383a482e04fe
Sean Hellum <seanhellum45@gmail.com>
2020-01-27 21:55:42 +0000

    update red() implementation

--- examples/hi.rs
+++ examples/hi.rs
@@ -1,4 +1,4 @@
 use cli_kit;
 fn main() {
-    println!("{}", cli_kit::ansi::color_codes::Red(5));
+    println!("{}", cli_kit::ansi::color_codes::red(5));
 }
--- src/ansi/color_codes.rs
+++ src/ansi/color_codes.rs
@@ -1,3 +1,5 @@
-pub fn Red<T: std::string::ToString>(text: T) -> String {   
+use std::string::ToString;
+
+pub fn red<T: ToString>(text: T) -> String {
     "\x1b[31m".to_owned() + &text.to_string() + "\x1b[0m"
 }
