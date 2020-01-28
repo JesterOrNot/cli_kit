fe1eea22c969f1460e5c7393dfdf62684f2583c1
Sean Hellum <seanhellum45@gmail.com>
2020-01-28 16:43:47 +0000

    document cursor functions

--- src/ansi/cursor.rs
+++ src/ansi/cursor.rs
@@ -1,6 +1,8 @@
+/// Hides the cursor by printing ANSI escape sequence
 pub fn hide_cursor() {
     println!("\x1b[?25l");
 }
+/// Shows the cursor by printing ANSI escape sequence
 pub fn show_cursor() {
     println!("\x1b[?25h")
 }
