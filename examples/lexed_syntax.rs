e6e44dec43bc013274e07790449c3b157d365527
Sean Hellum <seanhellum45@gmail.com>
2020-02-01 22:23:26 +0000

    minor update to 'lexed_synax' formatting

--- examples/lexed_syntax.rs
+++ examples/lexed_syntax.rs
@@ -1,22 +1,15 @@
 extern crate cli_kit;
-extern crate rustyline;
+use cli_kit::ansi::color_codes::{cyan, green, magenta};
 use std::convert::From;
-use std::iter::Peekable;
 
 fn main() {
     let mut colored_version = String::new();
     let lexed_input = lex(&"2 + 3 - 65 + 3 ([]) ffff fff f".to_owned());
     for i in lexed_input.unwrap() {
         match i {
-            LexItem::Num(n) => {
-                colored_version += &cli_kit::ansi::color_codes::magenta(n, false, false).to_string()
-            }
-            LexItem::Op(n) => {
-                colored_version += &cli_kit::ansi::color_codes::cyan(n, false, false).to_string()
-            }
-            LexItem::Paren(n) => {
-                colored_version += &cli_kit::ansi::color_codes::green(n, false, false).to_string()
-            }
+            LexItem::Num(n) => colored_version += &magenta(n, false, false).to_string(),
+            LexItem::Op(n) => colored_version += &cyan(n, false, false).to_string(),
+            LexItem::Paren(n) => colored_version += &green(n, false, false).to_string(),
             LexItem::Whitespace(_) | LexItem::Char(_) => colored_version += &String::from(i),
         };
     }
