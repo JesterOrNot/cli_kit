9a9460ce6252199db53ee2b76d7ed277a028ca31
Sean Hellum <seanhellum45@gmail.com>
2020-02-03 20:26:18 +0000

    add clear screen method

--- src/ansi/cursor.rs
+++ src/ansi/cursor.rs
@@ -6,3 +6,26 @@ pub fn hide_cursor() {
 pub fn show_cursor() {
     print!("\x1b[?25h")
 }
+
+/// Clears the screen by printing ANSI escapse sequence
+///
+/// Paramaters
+///
+/// degree - how much is the screen cleared
+///
+/// 0 = clear from cursor to the end
+///
+/// 1 = clear from cursor to the start
+///
+/// 2 = clear the entire screen
+///
+/// Example
+/// ```
+/// use cli_kit::ansi::cursor::clear_screen;
+/// fn main() {
+///     clear_screen(2); // clears the entire screen
+/// }
+/// ```
+pub fn clear_screen(degree: i32) {
+    print!("\x1b[{}K", degree)
+}
